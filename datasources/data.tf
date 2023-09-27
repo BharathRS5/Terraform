d57ddeb1779b0f8d8ce2964c389a30887956619a
RSB <rsbharathrsb@gmail.com>
2023-09-27 13:21:05 +0530

    terraform

--- datasources/data.tf
+++ datasources/data.tf
@@ -12,3 +12,35 @@ data "aws_ami" "to-get-ami-id" {
 output "ami_id" {
     value = data.aws_ami.to-get-ami-id.id
 }
+
+data "aws_vpc" "to-get-details-of-default-vpc" {
+  default = true
+}
+
+output "vpc_info" {
+  value = data.aws_vpc.to-get-details-of-default-vpc
+}
+
+resource "aws_security_group" "allow-all" {
+    name = "allow"
+    vpc_id = data.aws_vpc.to-get-details-of-default-vpc.id
+    description = "allowing all ports"
+
+    ingress {
+        description      = "allowing all inbound traffic"
+        from_port        = 0
+        to_port          = 0
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        
+    }
+
+     egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1" # -1 means all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+        
+    }
+  
+}
